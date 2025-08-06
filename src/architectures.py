e7ca299bc615a97c975628f603b7639e38df003f
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2025-08-06 11:00:04 -0400

    Implemented ViT and MalConv for multiple and single channel discrete sequences.

---
+++ src/architectures.py
@@ -0,0 +1,403 @@
+"""
+Neural architectures and their components.
+
+Notations:
+    B: Batch size
+    T: Sequence length
+    N: Number of patches
+    P: Patch size
+    E: Embedding dimension
+    C: Number of channels
+    S: Post-conv length
+    H: Hidden dimension
+    M: Number of classes
+"""
+
+import math
+import sys
+from typing import Optional, Protocol
+
+import torch
+from torch import nn
+from torch import Tensor
+from torch import IntTensor
+from torch import FloatTensor
+from torch.nn.utils.rnn import pad_sequence
+
+
+class TensorError(ValueError):
+    """
+    Exception raised when the shape of a tensor does not match the expected shape.
+    """
+
+    def __init__(self, x: Tensor, s: Optional[tuple[Optional[int], ...]], t: Optional[torch.dtype]) -> None:
+        super().__init__(
+            f"Expected tensor with dtype {t} and shape {s}. Got tensor with dtype {x.dtype} and shape {tuple(x.shape)}."
+        )
+
+    @staticmethod
+    def check(x: Tensor, s: Optional[tuple[Optional[int], ...]], t: Optional[torch.dtype]) -> None:
+        if t is not None and x.dtype != t:
+            raise TensorError(x, s, t)
+        if s is not None and len(x.shape) != len(s):
+            raise TensorError(x, s, t)
+        if s is not None:
+            for i, j in zip(x.shape, s, strict=True):
+                if j is not None and i != j:
+                    raise TensorError(x, s, t)
+
+
+class ClassifificationHead(nn.Module):
+    """
+    Classification head for a neural network.
+    """
+
+    def __init__(
+        self,
+        input_size: int,
+        num_classes: int,
+        hidden_size: int = -1,
+        num_layers: int = 1,
+        dropout: float = 0.1,
+    ) -> None:
+        super().__init__()
+
+        if num_layers < 1:
+            raise ValueError("Number of layers must be at least 1.")
+        if num_layers > 1 and hidden_size <= 0:
+            raise ValueError("Hidden size must be positive for multi-layer heads.")
+
+        def create_hidden_layer(in_size: int) -> list[nn.Module]:
+            return [
+                nn.Linear(in_size, hidden_size),
+                nn.Dropout(dropout),
+                nn.LeakyReLU(),
+            ]
+
+        self.layers = nn.Sequential()
+        for i in range(num_layers - 1):
+            layers = create_hidden_layer(input_size if i == 0 else hidden_size)
+            for layer in layers:
+                self.layers.append(layer)
+        self.layers.append(nn.Linear(hidden_size if num_layers > 1 else input_size, num_classes))
+
+        self.input_size = input_size
+        self.num_classes = num_classes
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.dropout = dropout
+
+    def forward(self, x: FloatTensor) -> FloatTensor:
+        """
+        Args:
+            x: Input tensor of shape (B, H).
+        Returns:
+            Output tensor of shape (B, M).
+        """
+        TensorError.check(x, (None, self.input_size), torch.float)
+
+        z = self.layers.forward(x)
+
+        return z
+
+
+class MultiChannelDiscreteSequenceClassifier(Protocol):
+    """
+    Multi-channel classifier for discrete sequence inputs.
+    """
+
+    def __init__(self, num_embeddings: list[int], embedding_dim: list[int], num_classes: int) -> None: ...
+
+    def forward(self, *x: IntTensor) -> FloatTensor:
+        """
+        Args:
+            x: Input tensor(s) of shape (B, T).
+        Returns:
+            Output tensor of shape (B, C).
+        """
+        ...
+
+
+class LatentPooler(Protocol):
+    """
+    Pooling layer for latent representations.
+    """
+
+    def forward(self, z: FloatTensor) -> FloatTensor:
+        """
+        Args:
+            z: Input tensor of shape (B, T, E).
+        Returns:
+            Output tensor of shape (B, E).
+        """
+        ...
+
+
+class MeanLatentPooler(nn.Module):
+    """
+    Mean pooling layer for latent representations.
+    """
+
+    def forward(self, z: FloatTensor) -> FloatTensor:
+        """
+        Args:
+            z: Input tensor of shape (B, T, E).
+        Returns:
+            Output tensor of shape (B, E).
+        """
+        TensorError.check(z, (None, None, None), torch.float)
+        return z.mean(dim=1)
+
+
+class MultiChannelDiscreteEmbedding(nn.Module):
+    """
+    Multi-channel embedding layer for discrete sequence inputs.
+    """
+
+    def __init__(self, num_embeddings: list[int], embedding_dim: list[int]) -> None:
+        super().__init__()
+
+        self.embedding = nn.ModuleList()
+        for num_embeddings_, embedding_dim_ in zip(num_embeddings, embedding_dim, strict=True):
+            embedding = nn.Embedding(num_embeddings_, embedding_dim_)
+            self.embedding.append(embedding)
+
+    @property
+    def max_length(self) -> int:
+        return sys.maxsize
+
+    @property
+    def min_length(self) -> int:
+        return 0
+
+    def forward(self, *x: IntTensor) -> FloatTensor:
+        """
+        Args:
+            x: Input tensor(s) of shape (B, T).
+        Returns:
+            Output tensor of shape (B, T, E).
+        """
+        for x_ in x:
+            TensorError.check(x_, tuple(x[0].shape), torch.int64)
+
+        z = torch.cat([self.embedding[i].forward(x[i]) for i in range(len(x))], dim=-1)
+
+        return z
+
+
+class SequenceEmbeddingEncoder(nn.Module):
+    """
+    Patch embedding layer for discrete sequence inputs.
+    """
+
+    def __init__(
+        self, patch_size: int, in_channels: int, out_channels: int, kernel_size: int = 64, stride: int = 64
+    ) -> None:
+        super().__init__()
+
+        if patch_size < kernel_size:
+            raise ValueError("Patch size must be greater than or equal to kernel size.")
+
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, stride)
+        self.pool = nn.AdaptiveMaxPool1d(1)
+
+        self.patch_size = patch_size
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+    @property
+    def max_length(self) -> int:
+        return sys.maxsize
+
+    @property
+    def min_length(self) -> int:
+        return self.kernel_size
+
+    def forward(self, z: FloatTensor) -> FloatTensor:
+        """
+        Args:
+            x: Input tensor of shape (B, T, E).
+        Returns:
+            Output tensor of shape (B, N, C).
+        """
+        TensorError.check(z, (None, None, None), torch.float)
+
+        B = z.shape[0]
+        T = z.shape[1]
+        E = z.shape[2]
+        P = min(self.patch_size, T)
+        N = math.ceil(T / P)
+        C = self.out_channels
+        S = math.floor((T - self.kernel_size) / self.stride + 1)
+
+        if T < self.min_length:
+            raise RuntimeError(f"Input sequence length {T} is less than the minimum required length {self.min_length}.")
+
+        z = self.split_patches(z)  # (B,  N, P, E)
+        z = z.reshape(B * N, P, E)  # (BN, P, E)
+        z = z.permute(0, 2, 1)  # (BN, E, P)
+        z = self.conv.forward(z)  # (BN, C, S)
+        z = self.pool.forward(z)  # (BN, C, 1)
+        z = z.squeeze(-1)  # (BN, C)
+        z = z.reshape(B, N, C)  # (B,  N, C)
+
+        return z
+
+    @staticmethod
+    def _split_patches(z: FloatTensor, patch_size: int) -> FloatTensor:
+        """
+        Args:
+            z: Input tensor of shape (B, T, E).
+        Returns:
+            Output tensor of shape (B, N, P, E).
+        """
+        TensorError.check(z, (None, None, None), torch.float)
+
+        if z.shape[1] <= patch_size:
+            return z.unsqueeze(1)
+
+        patches = torch.split(z.permute(1, 0, 2), patch_size)  # N x (P, B, E)
+        patches = pad_sequence(patches, batch_first=True)  # (N, P, B, E)
+        patches = patches.permute(2, 0, 1, 3)  # (B, N, P, E)
+
+        return patches
+
+    def split_patches(self, z: FloatTensor) -> FloatTensor:
+        """
+        Args:
+            z: Input tensor of shape (B, T, E).
+        Returns:
+            Output tensor of shape (B, N, P, E).
+        """
+        TensorError.check(z, (None, None, self.in_channels), torch.float)
+        return self._split_patches(z, self.patch_size)
+
+
+class MultiChannelDiscreteSequenceVisionTransformer(nn.Module):
+    """
+    Vision Transformer for discrete sequential inputs.
+    """
+
+    def __init__(
+        self,
+        num_embeddings: list[int],
+        embedding_dim: list[int],
+        patch_size: int,
+        d_model: int,
+        nhead: int,
+        num_layers: int = 1,
+        num_classes: int = 2,
+    ) -> None:
+        super().__init__()
+
+        self.embedding = MultiChannelDiscreteEmbedding(num_embeddings, embedding_dim)
+        self.encoder = SequenceEmbeddingEncoder(patch_size, sum(embedding_dim), d_model)
+        layer = nn.TransformerEncoderLayer(d_model, nhead, 4 * d_model, activation="gelu", batch_first=True)
+        self.backbone = nn.TransformerEncoder(layer, num_layers, norm=nn.RMSNorm(d_model))
+        self.pool = MeanLatentPooler()
+        self.head = ClassifificationHead(d_model, num_classes)
+
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = sum(embedding_dim)
+        self.patch_size = patch_size
+        self.d_model = d_model
+        self.nhead = nhead
+        self.num_layers = num_layers
+        self.num_classes = num_classes
+
+    @property
+    def max_length(self) -> int:
+        mods = [self.embedding, self.encoder, self.backbone, self.head]
+        return min(getattr(mod, "max_length", 0) for mod in [mods])
+
+    @property
+    def min_length(self) -> int:
+        mods = [self.embedding, self.encoder, self.backbone, self.head]
+        return max(getattr(mod, "min_length", 0) for mod in [mods])
+
+    def forward(self, *x: IntTensor) -> FloatTensor:
+        """
+        Args:
+            x: Input tensor(s) of shape (B, T).
+        Returns:
+            Output tensor of shape (B, M).
+        """
+        for x_ in x:
+            TensorError.check(x_, tuple(x[0].shape), torch.int64)
+
+        print(x[0].shape)
+        z = self.embedding.forward(*x)  # (B, T, E)
+        z = self.encoder.forward(z)  # (B, N, H)
+        z = self.backbone.forward(z)  # (B, N, H)
+        z = self.pool.forward(z)  # (B, H)
+        z = self.head.forward(z)  # (B, M)
+
+        return z
+
+
+class MultiChannelMalConv(nn.Module):
+    """
+    MalConv model for discrete sequential inputs.
+    """
+
+    def __init__(
+        self,
+        num_embeddings: list[int],
+        embedding_dim: list[int],
+        out_channels: int = 128,
+        kernel_size: int = 512,
+        stride: int = 512,
+        num_classes: int = 2,
+    ) -> None:
+        super().__init__()
+
+        self.embedding = MultiChannelDiscreteEmbedding(num_embeddings, embedding_dim)
+        self.conv_1 = nn.Conv1d(sum(embedding_dim), out_channels, kernel_size, stride)
+        self.conv_2 = nn.Conv1d(sum(embedding_dim), out_channels, kernel_size, stride)
+        self.pool = nn.AdaptiveMaxPool1d(1)
+        self.sigmoid = nn.Sigmoid()
+        self.head = ClassifificationHead(out_channels, num_classes)
+
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = sum(embedding_dim)
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.num_classes = num_classes
+
+    @property
+    def max_length(self) -> int:
+        return sys.maxsize
+
+    @property
+    def min_length(self) -> int:
+        return self.kernel_size
+
+    def forward(self, *x: IntTensor) -> FloatTensor:
+        """
+        Args:
+            x: Input tensor of shape (B, T).
+        Returns:
+            Output tensor of shape (B, M).
+        """
+        for x_ in x:
+            TensorError.check(x_, tuple(x[0].shape), torch.int64)
+
+        B = x[0].shape[0]
+        T = x[0].shape[1]
+        E = self.embedding_dim
+        C = self.out_channels
+        S = math.floor((T - self.kernel_size) / self.stride + 1)
+
+        z = self.embedding.forward(*x)  # [B, L, E]
+        z = z.transpose(1, 2)  # [B, E, L]
+        c_1 = self.conv_1.forward(z)  # [B, C, S - 1]
+        c_2 = self.conv_2.forward(z)  # [B, C, S - 1]
+        g = c_1 * self.sigmoid.forward(c_2)  # [B, C, S - 1]
+        z = self.pool.forward(g)  # [B, C, 1]
+        z = z.squeeze(-1)  # [B, C]
+        z = self.head.forward(z)  # [B, M]
+
+        return z
---
+++ tests/test_architectures.py
@@ -0,0 +1,245 @@
+"""
+Tests.
+"""
+
+import math
+
+import pytest
+import torch
+
+from src.architectures import TensorError
+from src.architectures import ClassifificationHead
+from src.architectures import MultiChannelDiscreteEmbedding
+from src.architectures import MultiChannelDiscreteSequenceVisionTransformer
+from src.architectures import MultiChannelMalConv
+from src.architectures import SequenceEmbeddingEncoder
+
+
+class TestTensorError:
+    def test_check_good(self):
+        TensorError.check(torch.randn(4, 5), (None, 5), torch.float)
+        TensorError.check(torch.randn(4, 5), (None, None), None)
+        TensorError.check(torch.randn(4, 5), (4, 5), torch.float)
+
+    def test_check_bad_dtype(self):
+        with pytest.raises(TensorError):
+            TensorError.check(torch.randn(4, 5), (None, 5), torch.int)
+        with pytest.raises(TensorError):
+            TensorError.check(torch.randn(4, 5), (None, None), torch.int)
+        with pytest.raises(TensorError):
+            TensorError.check(torch.randn(4, 5), (4, 5), torch.int)
+
+    def test_check_bad_shape(self):
+        with pytest.raises(TensorError):
+            TensorError.check(torch.randn(4, 5), (None, 3), torch.float)
+        with pytest.raises(TensorError):
+            TensorError.check(torch.randn(4, 5), (None,), None)
+        with pytest.raises(TensorError):
+            TensorError.check(torch.randn(4, 5), (4, 5, None), torch.float)
+
+
+class TestClassificationHead:
+    B = 4
+
+    @pytest.mark.parametrize("input_size", [8, 16, 32])
+    @pytest.mark.parametrize("num_classes", [2, 4, 8])
+    @pytest.mark.parametrize("hidden_size", [-1, 8, 16])
+    @pytest.mark.parametrize("num_layers", [1, 2, 3])
+    def test_forward(self, input_size: int, num_classes: int, hidden_size: int, num_layers: int):
+        if num_layers > 1 and hidden_size <= 0:
+            with pytest.raises(ValueError):
+                ClassifificationHead(input_size, num_classes, hidden_size, num_layers)
+            return
+
+        model = ClassifificationHead(input_size, num_classes, hidden_size, num_layers)
+        z = torch.randn(self.B, input_size)
+        z = model.forward(z)
+        assert z.shape == (self.B, num_classes)
+
+
+class TestMultiChannelDiscreteEmbedding:
+    B = 4
+    T = 256
+
+    @pytest.mark.parametrize("num_embedding", [[8], [16, 32], [8, 16, 32]])
+    @pytest.mark.parametrize("embedding_dim", [[4], [8, 16], [4, 8, 16]])
+    def test_forward(self, num_embedding: list[int], embedding_dim: list[int]):
+        if len(num_embedding) != len(embedding_dim):
+            with pytest.raises(ValueError):
+                MultiChannelDiscreteEmbedding(num_embedding, embedding_dim)
+            return
+
+        model = MultiChannelDiscreteEmbedding(num_embedding, embedding_dim)
+        x = [torch.randint(0, v, (self.B, self.T), dtype=torch.long) for v in num_embedding]
+        z = model.forward(*x)
+        assert z.shape == (self.B, self.T, sum(embedding_dim))
+
+        if len(num_embedding) > 1:
+            x = [torch.randint(0, v, (self.B, self.T - i), dtype=torch.long) for i, v in enumerate(num_embedding)]
+            with pytest.raises(TensorError):
+                model.forward(*x)
+
+
+class TestSequenceEmbeddingEncoder:
+    B = 4
+
+    @pytest.mark.parametrize("batch_size", [1, 2, 3, 5])
+    @pytest.mark.parametrize("seq_length", [1, 2, 3, 11, 17, 53])
+    @pytest.mark.parametrize("patch_size", [1, 2, 3, 11, 17, 53])
+    @pytest.mark.parametrize("in_channels", [3, 5, 7])
+    def test_split_patches(self, batch_size: int, seq_length: int, patch_size: int, in_channels: int):
+        z = torch.rand(batch_size, seq_length, in_channels)
+        patches = SequenceEmbeddingEncoder._split_patches(z, patch_size)
+        assert patches.shape[0] == batch_size
+        assert patches.shape[1] == math.ceil(seq_length / patch_size)
+        assert patches.shape[2] == min(patch_size, seq_length)
+        assert patches.shape[3] == in_channels
+
+    def _test_forward(
+        self, seq_length: int, patch_size: int, in_channels: int, out_channels: int, kernel_size: int, stride: int
+    ):
+        model = SequenceEmbeddingEncoder(patch_size, in_channels, out_channels, kernel_size, stride)
+        z = torch.rand(self.B, seq_length, in_channels)
+        z = model.forward(z)
+        assert z.shape[0] == self.B
+        assert z.shape[1] == math.ceil(seq_length / patch_size)
+        assert z.shape[2] == out_channels
+
+    @pytest.mark.parametrize("seq_length", [11, 17, 53])
+    @pytest.mark.parametrize("patch_size", [11, 17, 53])
+    @pytest.mark.parametrize("in_channels", [3, 5, 7])
+    @pytest.mark.parametrize("out_channels", [8, 16, 32])
+    @pytest.mark.parametrize("kernel_size", [3, 5])
+    @pytest.mark.parametrize("stride", [1, 2])
+    def test_forward(
+        self, seq_length: int, patch_size: int, in_channels: int, out_channels: int, kernel_size: int, stride: int
+    ):
+        self._test_forward(seq_length, patch_size, in_channels, out_channels, kernel_size, stride)
+
+    @pytest.mark.parametrize("seq_length", [1, 2, 3, 11, 17, 63])
+    def test_forward_seq_length_too_short(self, seq_length: int):
+        # Any sequence length less than the kernel size should raise an error.
+        with pytest.raises(RuntimeError):
+            self._test_forward(seq_length, patch_size=64, in_channels=16, out_channels=12, kernel_size=64, stride=4)
+
+    @pytest.mark.parametrize("patch_size", [1, 2, 3, 11, 17, 63])
+    def test_forward_patch_size_too_small(self, patch_size: int):
+        # Any patch size less than the kernel size should raise an error.
+        with pytest.raises(ValueError):
+            self._test_forward(
+                seq_length=1024, patch_size=patch_size, in_channels=16, out_channels=12, kernel_size=64, stride=4
+            )
+
+
+class TestMultiChannelDiscreteSequenceVisionTransformer:
+    B = 2
+
+    def _test_forward(
+        self,
+        seq_length: int,
+        num_embeddings: list[int],
+        embedding_dim: list[int],
+        patch_size: int,
+        d_model: int,
+        nhead: int,
+        num_layers: int,
+        num_classes: int,
+    ):
+        model = MultiChannelDiscreteSequenceVisionTransformer(
+            num_embeddings, embedding_dim, patch_size, d_model, nhead, num_layers, num_classes
+        )
+        x = [torch.randint(0, v, (self.B, seq_length), dtype=torch.long) for v in num_embeddings]
+        y = model.forward(*x)
+        assert y.shape == (self.B, num_classes)
+
+    @pytest.mark.parametrize("seq_length", [359, 512])
+    @pytest.mark.parametrize("num_embeddings", [[8], [16, 32], [8, 16, 32]])
+    @pytest.mark.parametrize("embedding_dim", [[4], [8, 16], [4, 8, 16]])
+    @pytest.mark.parametrize("patch_size", [255, 256, 257])
+    @pytest.mark.parametrize("d_model", [16, 32])
+    @pytest.mark.parametrize("nhead", [1, 2])
+    @pytest.mark.parametrize("num_layers", [1, 2])
+    @pytest.mark.parametrize("num_classes", [2, 4, 8])
+    def test_forward(
+        self,
+        seq_length: int,
+        num_embeddings: list[int],
+        embedding_dim: list[int],
+        patch_size: int,
+        d_model: int,
+        nhead: int,
+        num_layers: int,
+        num_classes: int,
+    ):
+        if len(num_embeddings) != len(embedding_dim):
+            return
+        self._test_forward(
+            seq_length, num_embeddings, embedding_dim, patch_size, d_model, nhead, num_layers, num_classes
+        )
+
+    def test_forward_patch_size_too_small(self):
+        with pytest.raises(ValueError):
+            self._test_forward(
+                seq_length=1024,
+                num_embeddings=[8],
+                embedding_dim=[8],
+                patch_size=63,
+                d_model=64,
+                nhead=1,
+                num_layers=1,
+                num_classes=1,
+            )
+
+
+class TestMultiChannelMalConv:
+    B = 2
+
+    def _test_forward(
+        self,
+        seq_length: int,
+        num_embeddings: list[int],
+        embedding_dim: list[int],
+        out_channels: int,
+        kernel_size: int,
+        stride: int,
+        num_classes: int,
+    ):
+        model = MultiChannelMalConv(num_embeddings, embedding_dim, out_channels, kernel_size, stride, num_classes)
+        x = [torch.randint(0, v, (self.B, seq_length), dtype=torch.long) for v in num_embeddings]
+        y = model.forward(*x)
+        assert y.shape == (self.B, num_classes)
+
+    @pytest.mark.parametrize("seq_length", [17, 53, 256])
+    @pytest.mark.parametrize("num_embeddings", [[8], [16, 32], [8, 16, 32]])
+    @pytest.mark.parametrize("embedding_dim", [[4], [8, 16], [4, 8, 16]])
+    @pytest.mark.parametrize("out_channels", [16, 32])
+    @pytest.mark.parametrize("kernel_size", [3, 5])
+    @pytest.mark.parametrize("stride", [1, 2])
+    @pytest.mark.parametrize("num_classes", [1, 2, 3])
+    def test_forward(
+        self,
+        seq_length: int,
+        num_embeddings: list[int],
+        embedding_dim: list[int],
+        out_channels: int,
+        kernel_size: int,
+        stride: int,
+        num_classes: int,
+    ):
+        if len(num_embeddings) != len(embedding_dim):
+            return
+        self._test_forward(seq_length, num_embeddings, embedding_dim, out_channels, kernel_size, stride, num_classes)
+
+    @pytest.mark.parametrize("seq_length", [1, 2, 3, 11, 17, 63])
+    def test_forward_seq_length_too_short(self, seq_length: int):
+        # Any sequence length less than the kernel size should raise an error.
+        with pytest.raises(RuntimeError):
+            self._test_forward(
+                seq_length,
+                num_embeddings=[8],
+                embedding_dim=[8],
+                out_channels=64,
+                kernel_size=64,
+                stride=1,
+                num_classes=1,
+            )
