5f2474fed9beaf95abe108b815ceb4aadc7c5c2b
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2025-08-07 17:36:12 -0400

    Implemented two semantic guides.

--- src/binanal.py
+++ src/binanal.py
@@ -2,6 +2,7 @@
 Binary analysis.
 """
 
+from dataclasses import dataclass
 import hashlib
 from pathlib import Path
 import struct
@@ -9,6 +10,9 @@ from typing import Optional
 
 import lief
 import numpy as np
+import numpy.typing as npt
+from numpy.lib.stride_tricks import sliding_window_view
+from scipy.stats import entropy
 
 
 # Machine types, from most to least common for PE files.
@@ -32,6 +36,16 @@ for v in lief.PE.OptionalHeader.SUBSYSTEM.__dict__.values():
     if type(v).__name__ == "SUBSYSTEM" and v not in SUBSYSTEMS:
         SUBSYSTEMS.append(v)
 SUBSYSTEMS = tuple(SUBSYSTEMS)
+
+
+
+def is_section_executable(section: lief.PE.Section) -> bool:
+    for c in section.characteristics_lists:
+        if "MEM_EXECUTE" in c:
+            return True
+        if "CNT_CODE" in c:
+            return True
+    return False
 
 
 def is_dotnet(raw: str | Path | bytes) -> bool:
@@ -121,3 +135,134 @@ def rearm_disarmed_binary(src: str | Path | bytes, sha: str, check: bool = True,
                 return patched
 
     raise RuntimeError(f"Could not find matching machine and subsystem for SHA-256: {sha}")
+
+
+@dataclass(frozen=True)
+class SemanticSample:
+    buffer: bytes
+    parse: Optional[npt.NDArray[np.int32]] = None
+    entropy: Optional[npt.NDArray[np.float64]] = None
+    characteristics: Optional[npt.NDArray[np.int32]] = None
+
+    def __post_init__(self) -> None:
+        lengths = [len(x) for x in (self.buffer, self.parse, self.entropy, self.characteristics) if x is not None]
+        if len(set(lengths)) > 1:
+            raise ValueError(f"SemanticSample buffers have different lengths: {lengths}")
+
+
+class SemanticGuider:
+    """
+    Semantic guides to acompany a byte stream.
+    """
+
+    CHARACTERISTICS = [
+        # lief.PE.Section.CHARACTERISTICS.TYPE_NO_PAD,
+        lief.PE.Section.CHARACTERISTICS.CNT_CODE,
+        lief.PE.Section.CHARACTERISTICS.CNT_INITIALIZED_DATA,
+        lief.PE.Section.CHARACTERISTICS.CNT_UNINITIALIZED_DATA,
+        # lief.PE.Section.CHARACTERISTICS.LNK_OTHER,
+        # lief.PE.Section.CHARACTERISTICS.LNK_INFO,
+        # lief.PE.Section.CHARACTERISTICS.LNK_REMOVE,
+        # lief.PE.Section.CHARACTERISTICS.LNK_COMDAT,
+        lief.PE.Section.CHARACTERISTICS.GPREL,
+        # lief.PE.Section.CHARACTERISTICS.MEM_PURGEABLE,  # Reserved for future use
+        # lief.PE.Section.CHARACTERISTICS.MEM_16BIT,      # Reserved for future use
+        # lief.PE.Section.CHARACTERISTICS.MEM_LOCKED,     # Reserved for future use
+        # lief.PE.Section.CHARACTERISTICS.MEM_PRELOAD,    # Reserved for future use
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_1BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_2BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_4BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_8BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_16BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_32BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_64BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_128BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_256BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_512BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_1024BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_2048BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_4096BYTES,
+        # lief.PE.Section.CHARACTERISTICS.ALIGN_8192BYTES,
+        lief.PE.Section.CHARACTERISTICS.LNK_NRELOC_OVFL,
+        lief.PE.Section.CHARACTERISTICS.MEM_DISCARDABLE,
+        lief.PE.Section.CHARACTERISTICS.MEM_NOT_CACHED,
+        lief.PE.Section.CHARACTERISTICS.MEM_NOT_PAGED,
+        lief.PE.Section.CHARACTERISTICS.MEM_SHARED,
+        lief.PE.Section.CHARACTERISTICS.MEM_EXECUTE,
+        lief.PE.Section.CHARACTERISTICS.MEM_READ,
+        lief.PE.Section.CHARACTERISTICS.MEM_WRITE,
+    ]
+
+    def __init__(self, do_parse: bool = False, do_entropy: bool = False, do_characteristics: bool = False, window: int = 256) -> None:
+        self.do_parse = do_parse
+        self.do_entropy = do_entropy
+        self.do_characteristics = do_characteristics
+        self.window = window
+
+    def __call__(self, b: bytes) -> SemanticSample:
+        parse = None
+        if self.do_parse:
+            try:
+                parse = self.create_parse_guide(b)
+            except Exception as err:
+                print(f"Error creating parse guide: {err}")
+
+        entropy = None
+        if self.do_entropy:
+            try:
+                entropy = self.create_entropy_guide(b, self.window)
+            except Exception as err:
+                print(f"Error creating entropy guide: {err}")
+
+        characteristics = None
+        if self.do_characteristics:
+            try:
+                characteristics = self.create_characteristics_guide(b)
+            except Exception as err:
+                print(f"Error creating permission guide: {err}")
+
+        return SemanticSample(b, parse, entropy, characteristics)
+
+    @staticmethod
+    def create_parse_guide(b: bytes) -> npt.NDArray[np.int32]:
+        import warnings
+        warnings.warn("SemanticGuider.create_parse_guide is not implemented yet. Returning dummy data.")
+        return np.random.randint(0, 255, size=len(b), dtype=np.int32)
+
+    @staticmethod
+    def create_entropy_guide(b: bytes, w: int, e: float = 1e-8) -> npt.NDArray[np.float64]:
+        """
+        Args:
+            b: The byte stream to analyze.
+            w: The window size on each side for entropy calculation.
+            e: A small value to avoid division by zero in entropy calculation.
+        Returns:
+            A numpy array of entropy values for each byte in the stream. The beginning and end of the array
+            will be NaN-padded to account for the window size unless `w` is 0.
+        """
+        x = np.frombuffer(b, dtype=np.uint8).astype(np.float64)
+        x = entropy(sliding_window_view(x, w * 2 + 1) + e, axis=1, nan_policy="raise")
+        if np.any(np.isnan(x)):
+            raise RuntimeError("Entropy calculation resulted in NaN values.")
+        x: npt.NDArray[np.float64] = np.concatenate((np.full(w, np.nan), x, np.full(w, np.nan)))
+        return x
+
+    @staticmethod
+    def create_characteristics_guide(b: bytes) -> npt.NDArray[np.int32]:
+        """
+        Args:
+            b: The byte stream to analyze.
+        Returns:
+            A numpy array indicating the presence (1) or absense (0) of each characteristic in the
+            sections of the binary. Bytes outside of section boundaries will be set to -1.
+        """
+        x = np.full((len(b), len(SemanticGuider.CHARACTERISTICS)), -1, dtype=np.int32)
+        pe = lief.parse(b)
+        if pe is None:
+            raise RuntimeError("Failed to parse binary with lief.")
+        for section in pe.sections:
+            offset = section.offset
+            size = section.size
+            for i, c in enumerate(SemanticGuider.CHARACTERISTICS):
+                x[offset:offset + size, i] = 1 if section.has_characteristic(c) else 0
+        return x
--- tests/test_binanal.py
+++ tests/test_binanal.py
@@ -9,6 +9,8 @@ import numpy as np
 import pytest
 
 from src.binanal import patch_binary
+from src.binanal import SemanticGuider, SemanticSample
+
 
 FILES = sorted(Path("./tests/data").iterdir())
 
@@ -102,3 +104,76 @@ class TestPatchPEFile:
         equal = np.equal(np.frombuffer(org, dtype=np.uint8), np.frombuffer(new, dtype=np.uint8))
         # At most three bytes should differ
         assert np.sum(equal) >= len(equal) - 3, f"{np.sum(equal)} {len(equal)}"
+
+
+class TestSemanticGuider:
+
+    # @pytest.mark.parametrize("file", FILES)
+    # def test_create_parse_guide(self, file: Path):
+    #     x = SemanticGuider.create_parse_guide(file.read_bytes())
+    #     assert isinstance(x, np.ndarray)
+    #     assert x.ndim == 2
+    #     assert x.shape[1] == len(SemanticGuider.PARSE_GUIDE)
+    #     assert np.all(np.isin(x, [0, 1, -1]))
+
+    @pytest.mark.parametrize("w", [0, 16, 32])
+    @pytest.mark.parametrize("file", FILES)
+    def test_create_entropy(self, file: Path, w: int):
+        b = file.read_bytes()
+        print(len(b))
+        x = SemanticGuider.create_entropy_guide(b, w)
+        assert isinstance(x, np.ndarray)
+        assert x.ndim == 1
+        assert x.shape[0] == len(file.read_bytes())
+        assert np.all(np.isnan(x[:w])) or w == 0
+        assert np.all(np.isnan(x[-w + 1:])) or w == 0
+        assert np.all(np.isfinite(x[w:-w]))
+
+    @pytest.mark.parametrize("file", FILES)
+    def test_create_characteristics_guide(self, file: Path):
+        b = file.read_bytes()
+        x = SemanticGuider.create_characteristics_guide(b)
+        assert isinstance(x, np.ndarray)
+        assert x.ndim == 2
+        assert x.shape[0] == len(b)
+        assert x.shape[1] == len(SemanticGuider.CHARACTERISTICS)
+        assert np.all(np.isin(x, [0, 1, -1]))
+
+    @pytest.mark.parametrize("do_parse", [False, True])
+    @pytest.mark.parametrize("do_entropy", [False, True])
+    @pytest.mark.parametrize("do_characteristics", [False, True])
+    def test_main(self, do_parse: bool, do_entropy: bool, do_characteristics: bool):
+        file = FILES[0]
+        b = file.read_bytes()
+
+        guider = SemanticGuider(do_parse=do_parse, do_entropy=do_entropy, do_characteristics=do_characteristics)
+        sample = guider(b)
+        assert isinstance(sample, SemanticSample)
+
+        buffer = sample.buffer
+        assert isinstance(buffer, bytes)
+        assert buffer == b
+
+        parse = sample.parse
+        if do_parse:
+            assert isinstance(parse, np.ndarray)
+            assert parse.shape == (len(b),)
+            assert parse.dtype == np.int32
+        else:
+            assert parse is None
+
+        entropy = sample.entropy
+        if do_entropy:
+            assert isinstance(entropy, np.ndarray)
+            assert entropy.shape == (len(b),)
+            assert entropy.dtype == np.float64
+        else:
+            assert entropy is None
+
+        characteristics = sample.characteristics
+        if do_characteristics:
+            assert isinstance(characteristics, np.ndarray)
+            assert characteristics.shape == (len(b), len(SemanticGuider.CHARACTERISTICS))
+            assert characteristics.dtype == np.int32
+        else:
+            assert characteristics is None
