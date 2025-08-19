0d35691b9f2be0ee3eda99a1545a639a950d8779
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2025-08-19 11:17:12 -0400

    Implemented entropy module (from scratch).

---
+++ src/binentropy.py
@@ -0,0 +1,252 @@
+"""
+Sliding window entropy calculations for binary data.
+"""
+
+import os
+from enum import Enum
+
+import numpy as np
+from numpy import typing as npt
+import numba as nb
+import torch
+from torch import Tensor
+from torch import BoolTensor
+from torch import ByteTensor
+from torch import HalfTensor
+from torch import FloatTensor
+from torch import DoubleTensor
+
+
+AnyFloatTensor = HalfTensor | FloatTensor | DoubleTensor
+AnyFloatArray  = npt.NDArray[np.float16] | npt.NDArray[np.float32] | npt.NDArray[np.float64]
+
+
+class Backend(Enum):
+    """
+    Enum for specifying the backend to use for entropy calculations.
+    """
+    NUMPY = "numpy"
+    NUMBA = "numba"
+    TORCH = "torch"
+
+
+def is_integer_tensor(t: Tensor) -> bool:
+    return t.dtype in (torch.int8, torch.int16, torch.int32, torch.int64, torch.uint8)
+
+
+def isfinite(x: np.ndarray | Tensor) -> BoolTensor | npt.NDArray[np.bool_]:  # type: ignore[type-arg]
+    if isinstance(x, Tensor):
+        return torch.isfinite(x)
+    elif isinstance(x, np.ndarray):
+        return np.isfinite(x)
+    raise TypeError(f"Unsupported type: {type(x)}. Expected Tensor or ndarray.")
+
+
+def isnan(x: np.ndarray | Tensor) -> BoolTensor | npt.NDArray[np.bool_]:  # type: ignore[type-arg]
+    if isinstance(x, Tensor):
+        return torch.isnan(x)
+    elif isinstance(x, np.ndarray):
+        return np.isnan(x)
+    raise TypeError(f"Unsupported type: {type(x)}. Expected Tensor or ndarray.")
+
+
+def _get_numpy_array_from_input(input: str | os.PathLike[str] | bytes | memoryview | np.ndarray | torch.Tensor) -> npt.NDArray[np.uint8]:  # type: ignore[type-arg]
+    x: npt.NDArray[np.uint8]
+
+    if isinstance(input, (str, os.PathLike)):
+        x = np.fromfile(input, dtype=np.uint8)  # type: ignore[no-untyped-call]
+    elif isinstance(input, (bytes, memoryview)):
+        x = np.frombuffer(input, dtype=np.uint8)  # type: ignore[no-untyped-call]
+    elif isinstance(input, torch.Tensor) and is_integer_tensor(input) and input.ndim == 1:
+        x = input.numpy(force=True).astype(np.uint8)
+    elif isinstance(input, np.ndarray) and np.issubdtype(input.dtype, np.integer) and input.ndim == 1:
+        x = input.astype(np.uint8)
+    else:
+        raise TypeError(
+            f"Unsupported input type: {type(input)}"
+            f"{' or shape/dtype ' + str(tuple(input.shape)) + ' ' + str(input.dtype) if isinstance(input, (np.ndarray, Tensor)) else ''}"
+            "."
+        )
+
+    if x.ndim != 1:
+        raise ValueError(f"Unsupported input shape: {x.shape}. Expected 1D array.")
+
+    return x
+
+
+def _get_torch_array_from_input(input: str | os.PathLike[str] | bytes | memoryview | np.ndarray | torch.Tensor) -> ByteTensor:  # type: ignore[type-arg]
+    x: ByteTensor
+
+    if isinstance(input, (str, os.PathLike)):
+        x = torch.from_file(input, shared=False, dtype=torch.uint8, size=os.path.getsize(input))
+    elif isinstance(input, (bytes, memoryview)):
+        x = torch.frombuffer(input, dtype=torch.uint8)
+    elif isinstance(input, np.ndarray) and np.issubdtype(input.dtype, np.integer) and input.ndim == 1:
+        x = torch.from_numpy(input).to(torch.uint8)
+    elif isinstance(input, Tensor) and is_integer_tensor(input) and input.ndim == 1:
+        x = input.to(torch.uint8)
+    else:
+        raise TypeError(
+            f"Unsupported input type: {type(input)}"
+            f"{' or shape/dtype ' + str(tuple(input.shape)) + ' ' + str(input.dtype) if isinstance(input, (np.ndarray, Tensor)) else ''}"
+            "."
+        )
+
+    if x.ndim != 1:
+        raise ValueError(f"Unsupported input shape: {x.shape}. Expected 1D array.")
+
+    return x
+
+
+def compute_entropy(
+    input: str | os.PathLike[str] | bytes | memoryview | np.ndarray | torch.Tensor,  # type: ignore[type-arg]
+    backend: str | Backend,
+    *,
+    radius: int = 1,
+    stride: int = 1,
+) -> DoubleTensor | npt.NDArray[np.float64]:
+    """
+    Compute the sliding window entropy of the input data.
+
+    Args:
+        input: Input data representing raw bytes.
+        measure: Type of entropy measure to compute.
+        algorithm: Algorithm to use for entropy calculation.
+        backend: Backend to use for computation.
+        check: Whether to check the result for non-finite values.
+        radius: Radius of the sliding window, i.e., the number of bytes in either
+            direction that will contribute to a local entropy calculation.
+        stride: Stride of the sliding window, i.e., the number of bytes to skip
+            between consecutive entropy calculations.
+        padding: Value to use for padding on either side of the input data.
+
+    Returns:
+        Entropy values as a tensor or array within a sliding window of each byte in the input data.
+    """
+    backend = Backend(backend) if isinstance(backend, str) else backend
+
+    if backend == Backend.NUMPY:
+        x = _get_numpy_array_from_input(input)
+        h = compute_entropy_numpy(x, radius=radius, stride=stride)
+    elif backend == Backend.NUMBA:
+        x = _get_numpy_array_from_input(input)
+        h = compute_entropy_numba(x, radius=radius, stride=stride)
+    elif backend == Backend.TORCH:
+        x = _get_torch_array_from_input(input)
+        h = compute_entropy_torch(x, radius=radius, stride=stride)
+    else:
+        raise ValueError(f"Unsupported backend: {backend}.")
+
+    return h
+
+
+def _compute_discrete_entropy_from_counts_numpy(n: int, c: npt.NDArray[np.uint8]) -> float:
+    c = c[c > 0]
+    c = c.astype(np.float64)
+    h = np.log2(n) - (c * np.log2(c)).sum() / n
+    return float(h)
+
+
+def _compute_discrete_entropy_from_counts_torch(n: int, c: ByteTensor) -> float:
+    c = c[c > 0]
+    c = c.to(torch.float64)
+    h = torch.log2(torch.tensor(n, dtype=torch.float64)) - (c * torch.log2(c)).sum() / n
+    return float(h)
+
+
+def _compute_discrete_entropy_numpy(x: npt.NDArray[np.uint8]) -> float:
+    n = len(x)
+    c = np.bincount(x, minlength=256)
+    return _compute_discrete_entropy_from_counts_numpy(n, c)
+
+
+def _compute_discrete_entropy_torch(x: npt.NDArray[np.uint8]) -> float:
+    n = len(x)
+    c = torch.bincount(x, minlength=256)
+    return _compute_discrete_entropy_from_counts_torch(n, c)
+
+
+def _check_inputs(length: int, radius: int, stride: int) -> None:
+    if radius <= 0:
+        raise ValueError(f"Radius must be positive, got {radius}.")
+    if stride <= 0:
+        raise ValueError(f"Stride must be positive, got {stride}.")
+    if length <= 2 * radius + 1:
+        raise ValueError(f"Input data length {length} is too short for the specified radius {radius}.")
+    if stride > 2 * radius + 1:
+        raise ValueError(f"Stride {stride} is too large for the specified radius {radius}.")
+
+
+def compute_entropy_naive_numpy(x: npt.NDArray[np.uint8], r: int, s: int = 1) -> npt.NDArray[np.float64]:
+    _check_inputs(len(x), r, s)
+    w = 2 * r + 1
+    o = np.full((len(x),), float("nan"), dtype=np.float64)
+    # i is the index of the middle byte in the current window.
+    for i in range(r, len(x) - r, s):
+        l = i - r
+        u = i + r + 1
+        if len(x[l:u]) != w:
+            raise RuntimeError(f"Expected window length {w}, got {len(x[l:u])} at iteration {i}.")
+        h = _compute_discrete_entropy_numpy(x[l:u])
+        o[i:i + s] = h
+    return o
+
+
+def compute_entropy_naive_torch(x: ByteTensor, r: int, s: int = 1) -> DoubleTensor:
+    _check_inputs(len(x), r, s)
+    w = 2 * r + 1
+    o = torch.full((len(x),), float("nan"), dtype=torch.float64)
+    # i is the index of the middle byte in the current window.
+    for i in range(r, len(x) - r, s):
+        l = i - r
+        u = i + r + 1
+        if len(x[l:u]) != w:
+            raise RuntimeError(f"Expected window length {w}, got {len(x[l:u])} at iteration {i}.")
+        h = _compute_discrete_entropy_torch(x[l:u])
+        o[i:i + s] = h
+    return o
+
+
+def compute_entropy_rolling_numpy(x: npt.NDArray[np.uint8], r: int, s: int = 1) -> npt.NDArray[np.float64]:
+    _check_inputs(len(x), r, s)
+    w = 2 * r + 1
+    o = np.full((len(x),), float("nan"), dtype=np.float64)
+    c = np.bincount(x[0:w-s], minlength=256)
+    # i is one above the index of the last byte in the current window.
+    for i in range(w, len(x) + 1, s):
+        for j in range(i - s, i):
+            c[x[j]] += 1
+        for j in range(max(i - s - w, 0), i - w):
+            c[x[j]] -= 1
+        if len(np.nonzero(c)[0]) > w:
+            raise RuntimeError(f"Expected at most {w} unique values in the window, got {len(np.nonzero(c)[0])} at iteration {i}.")
+        if np.sum(c) != w:
+            raise RuntimeError(f"Expected sum of counts to be {w}, got {np.sum(c)} at iteration {i}.")
+        l = i - r - 1
+        u = i - r + s - 1
+        h = _compute_discrete_entropy_from_counts_numpy(w, c)
+        o[l:u] = h
+    return o
+
+
+def compute_entropy_rolling_torch(x: ByteTensor, r: int, s: int = 1) -> DoubleTensor:
+    _check_inputs(len(x), r, s)
+    w = 2 * r + 1
+    o = torch.full((len(x),), float("nan"), dtype=torch.float64)
+    c = torch.bincount(x[0:w-s], minlength=256)
+    # i is one above the index of the last byte in the current window.
+    for i in range(w, len(x) + 1, s):
+        for j in range(i - s, i):
+            c[x[j].item()] += 1
+        for j in range(max(i - s - w, 0), i - w):
+            c[x[j].item()] -= 1
+        print(c)
+        if len(torch.nonzero(c, as_tuple=True)[0]) > w:
+            raise RuntimeError(f"Expected at most {w} unique values in the window, got {len(torch.nonzero(c, as_tuple=True)[0])} at iteration {i}. {c}")
+        if torch.sum(c) != w:
+            raise RuntimeError(f"Expected sum of counts to be {w}, got {torch.sum(c)} at iteration {i}.")
+        l = i - r - 1
+        u = i - r + s - 1
+        h = _compute_discrete_entropy_from_counts_torch(w, c)
+        o[l:u] = h
+    return o
---
+++ tests/test_binentropy.py
@@ -0,0 +1,222 @@
+"""
+Tests.
+"""
+
+from pprint import pformat
+from typing import Callable
+
+import numpy as np
+from numpy import typing as npt
+import pytest
+import torch
+from torch import DoubleTensor
+
+from src.binentropy import compute_entropy_naive_numpy
+from src.binentropy import compute_entropy_rolling_numpy
+from src.binentropy import compute_entropy_naive_torch
+from src.binentropy import compute_entropy_rolling_torch
+from src.binentropy import _check_inputs
+
+
+# ---- helpers ---- #
+
+def pformat_isclose(a: np.ndarray, b: np.ndarray, equal_nan: bool = True) -> str:  # type: ignore[type-arg]
+    e = np.isclose(a, b, equal_nan=equal_nan)
+    a = a.tolist()
+    b = b.tolist()
+    e = list(map(lambda x: "==" if x else "!=", e.tolist()))
+    return pformat(list(zip(e, a, b)))
+
+
+def check_entropy(h: npt.NDArray[np.float64], size: int, radius: int, stride: int) -> None:
+    assert len(h) == size
+    nan_lo = radius
+    nan_up = size - radius + stride
+    assert np.isnan(h[:nan_lo]).all()
+    assert np.isnan(h[nan_up:]).all()
+    assert np.all(np.isfinite(h[radius:-radius])), pformat(np.isfinite(h[radius:-radius]).tolist())
+    assert np.all(h[radius:-radius] >= 0)
+    assert np.all(h[radius:-radius] <= np.log2(256))
+    idx = np.arange(radius, size - radius - stride + 1, stride)
+    for i in range(stride):
+        assert np.isclose(h[idx], h[idx + i], equal_nan=True).all(), pformat(np.isclose(h[idx], h[idx + i], equal_nan=True).tolist())
+
+# ---- tests ---- #
+
+SIZE = [15, 16, 17, 63, 64, 65, 255, 256, 257, 1023, 1024, 1025]
+RADIUS = [1, 2, 3, 15, 16, 17, 63, 64, 65, 255, 256, 257]
+STRIDE = [1, 2, 3, 4, 5, 6, 7]
+
+# ---- numpy ----- #
+
+@pytest.mark.parametrize("size", SIZE)
+@pytest.mark.parametrize("radius", RADIUS)
+@pytest.mark.parametrize("stride", STRIDE)
+def test_compute_entropy_naive_numpy(size: int, radius: int, stride: int) -> None:
+    should_raise = False
+    try:
+        _check_inputs(size, radius, stride)
+    except ValueError:
+        should_raise = True
+
+    b = np.random.randint(0, 256, size=size, dtype=np.uint8)
+    if should_raise:
+        with pytest.raises(ValueError):
+            compute_entropy_naive_numpy(b, radius, stride)
+        return
+
+    h = compute_entropy_naive_numpy(b, radius, stride)
+    check_entropy(h, size, radius, stride)
+
+
+@pytest.mark.parametrize("size", SIZE)
+@pytest.mark.parametrize("radius", RADIUS)
+@pytest.mark.parametrize("stride", STRIDE)
+def test_compute_entropy_rolling_numpy(size: int, radius: int, stride: int) -> None:
+    should_raise = False
+    try:
+        _check_inputs(size, radius, stride)
+    except ValueError:
+        should_raise = True
+
+    b = np.random.randint(0, 256, size=size, dtype=np.uint8)
+    if should_raise:
+        with pytest.raises(ValueError):
+            compute_entropy_rolling_numpy(b, radius, stride)
+        return
+
+    h = compute_entropy_rolling_numpy(b, radius, stride)
+    check_entropy(h, size, radius, stride)
+
+
+@pytest.mark.parametrize("size", SIZE)
+@pytest.mark.parametrize("radius", RADIUS)
+@pytest.mark.parametrize("stride", STRIDE)
+def test_compute_entropy_naive_and_rolling_numpy_equivalence(size: int, radius: int, stride: int) -> None:
+    should_raise = False
+    try:
+        _check_inputs(size, radius, stride)
+    except ValueError:
+        should_raise = True
+
+    b = np.random.randint(0, 256, size=size, dtype=np.uint8)
+    if should_raise:
+        with pytest.raises(ValueError):
+            compute_entropy_naive_numpy(b, radius, stride)
+        with pytest.raises(ValueError):
+            compute_entropy_rolling_numpy(b, radius, stride)
+        return
+
+    h_n = compute_entropy_naive_numpy(b, radius, stride)
+    h_r = compute_entropy_rolling_numpy(b, radius, stride)
+    check_entropy(h_n, size, radius, stride)
+    check_entropy(h_r, size, radius, stride)
+    assert np.isclose(h_n, h_r, equal_nan=True).all(), pformat_isclose(h_n, h_r, equal_nan=True)
+
+# ---- torch ---- #
+
+@pytest.mark.parametrize("size", SIZE)
+@pytest.mark.parametrize("radius", RADIUS)
+@pytest.mark.parametrize("stride", STRIDE)
+def test_compute_entropy_naive_torch(size: int, radius: int, stride: int) -> None:
+    should_raise = False
+    try:
+        _check_inputs(size, radius, stride)
+    except ValueError:
+        should_raise = True
+
+    b = torch.randint(0, 256, size=(size,), dtype=torch.uint8)
+    if should_raise:
+        with pytest.raises(ValueError):
+            compute_entropy_naive_torch(b, radius, stride)
+        return
+
+    h = compute_entropy_naive_torch(b, radius, stride).numpy()
+    check_entropy(h, size, radius, stride)
+
+
+@pytest.mark.parametrize("size", SIZE)
+@pytest.mark.parametrize("radius", RADIUS)
+@pytest.mark.parametrize("stride", STRIDE)
+def test_compute_entropy_rolling_torch(size: int, radius: int, stride: int) -> None:
+    should_raise = False
+    try:
+        _check_inputs(size, radius, stride)
+    except ValueError:
+        should_raise = True
+
+    b = torch.randint(0, 256, size=(size,), dtype=torch.uint8)
+    if should_raise:
+        with pytest.raises(ValueError):
+            compute_entropy_rolling_torch(b, radius, stride)
+        return
+
+    h = compute_entropy_rolling_torch(b, radius, stride).numpy()
+    check_entropy(h, size, radius, stride)
+
+
+@pytest.mark.parametrize("size", SIZE)
+@pytest.mark.parametrize("radius", RADIUS)
+@pytest.mark.parametrize("stride", STRIDE)
+def test_compute_entropy_naive_and_rolling_torch_equivalence(size: int, radius: int, stride: int) -> None:
+    should_raise = False
+    try:
+        _check_inputs(size, radius, stride)
+    except ValueError:
+        should_raise = True
+
+    b = torch.randint(0, 256, size=(size,), dtype=torch.uint8)
+    if should_raise:
+        with pytest.raises(ValueError):
+            compute_entropy_naive_torch(b, radius, stride)
+        with pytest.raises(ValueError):
+            compute_entropy_rolling_torch(b, radius, stride)
+        return
+
+    h_n = compute_entropy_naive_torch(b, radius, stride).numpy()
+    h_r = compute_entropy_rolling_torch(b, radius, stride).numpy()
+    check_entropy(h_n, size, radius, stride)
+    check_entropy(h_r, size, radius, stride)
+    assert np.isclose(h_n, h_r, equal_nan=True).all(), pformat_isclose(h_n, h_r, equal_nan=True)
+
+# ---- numba ---- #
+
+
+# ---- equiv ---- #
+
+@pytest.mark.parametrize("size", SIZE)
+@pytest.mark.parametrize("radius", RADIUS)
+@pytest.mark.parametrize("stride", STRIDE)
+def test_compute_entropy_numpy_and_torch_equivalence(size: int, radius: int, stride: int) -> None:
+    should_raise = False
+    try:
+        _check_inputs(size, radius, stride)
+    except ValueError:
+        should_raise = True
+
+    b_n = np.random.randint(0, 256, size=size, dtype=np.uint8)
+    b_t = torch.from_numpy(b_n)
+    if should_raise:
+        with pytest.raises(ValueError):
+            compute_entropy_naive_numpy(b_n, radius, stride)
+        with pytest.raises(ValueError):
+            compute_entropy_naive_torch(b_t, radius, stride)
+        return
+
+    h_n = compute_entropy_naive_numpy(b_n, radius, stride)
+    h_t = compute_entropy_rolling_torch(b_t, radius, stride).numpy()
+    check_entropy(h_n, size, radius, stride)
+    check_entropy(h_t, size, radius, stride)
+    assert np.isclose(h_n, h_t, equal_nan=True).all(), pformat_isclose(h_n, h_t, equal_nan=True)
+
+# ---- other ---- #
+
+def test_check_inputs_raises() -> None:
+    with pytest.raises(ValueError, match="Radius must be positive, got 0."):
+        _check_inputs(10, 0, 1)
+    with pytest.raises(ValueError, match="Stride must be positive, got 0."):
+        _check_inputs(10, 1, 0)
+    with pytest.raises(ValueError, match="Input data length 10 is too short for the specified radius 5."):
+        _check_inputs(10, 5, 1)
+    with pytest.raises(ValueError, match="Stride 10 is too large for the specified radius 4."):
+        _check_inputs(10, 4, 10)
