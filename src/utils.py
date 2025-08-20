370ea43433ec7c61f1b4b329fdc619381bb60302
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2025-08-20 15:00:07 -0400

    Implemented bit-packing utilities for 2D boolean tensors.

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import warnings
 
 import torch
 from torch import Tensor
+from torch import BoolTensor
 
 
 class TensorError(ValueError):
@@ -32,3 +33,55 @@ def check_tensor(x: Tensor, s: Optional[tuple[Optional[int], ...]] = None, t: Op
         for i, j in zip(x.shape, s, strict=True):
             if j is not None and i != j:
                 raise TensorError(x, s, t)
+
+
+def smallest_unsigned_integer_dtype(n: int) -> torch.dtype:
+    """
+    Returns the smallest unsigned integer dtype that can represent the given number of bits.
+    """
+    if n <= 8:
+        return torch.uint8
+    if n <= 16:
+        return torch.uint16
+    if n <= 32:
+        return torch.uint32
+    if n <= 64:
+        return torch.uint64
+    raise ValueError(f"Cannot represent {n} bits with an unsigned integer dtype.")
+
+
+def pack_bool_tensor(b: BoolTensor) -> Tensor:
+    """
+    Packs a two-dimensional boolean tensor into a one-dimensional integer tensor.
+
+    NOTE: unpack(pack(b)) == b, but pack(unpack(x)) != x.
+    """
+    check_tensor(b, (None, None), torch.bool)
+    if not (1 <= b.shape[1] <= 64):
+        raise ValueError(f"Cannot pack boolean tensor with {b.shape[1]} channels.")
+
+    # Use torch.int64 because shift operations are not implemented for most uint dtypes.
+    weights = (1 << torch.arange(b.shape[1], device=b.device, dtype=torch.int64))
+    x = (b.to(torch.int64) * weights).sum(dim=1)
+
+    # This mangles the value, but all we care about is the bits, so its okay.
+    dtype = smallest_unsigned_integer_dtype(b.shape[1])
+    return x.to(dtype)
+
+
+def unpack_bit_tensor(x: Tensor, d: int, dtype: torch.dtype = torch.bool) -> Tensor:
+    """
+    Unpacks a one-dimensional unsigned integer tensor into a two-dimensional boolean tensor.
+
+    NOTE: unpack(pack(b)) == b, but pack(unpack(x)) != x.
+    """
+    check_tensor(x, (None,), (torch.uint8, torch.uint16, torch.uint32, torch.uint64))
+    if not (1 <= d <= 64):
+        raise ValueError(f"Cannot unpack bit tensor with {d} channels.")
+
+    # Use torch.int64 because shift operations are not implemented for most uint dtypes.
+    x = x.to(torch.int64)
+    masks = (1 << torch.arange(d, device=x.device, dtype=torch.int64))
+    z = (x.unsqueeze(-1) & masks) != 0
+
+    return z.to(dtype)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,10 +3,14 @@ Tests.
 """
 
 import pytest
+import numpy as np
 import torch
 
 from src.utils import TensorError
 from src.utils import check_tensor
+from src.utils import pack_bool_tensor
+from src.utils import unpack_bit_tensor
+from src.utils import smallest_unsigned_integer_dtype
 
 
 class TestTensorError:
@@ -30,3 +34,42 @@ class TestTensorError:
             check_tensor(torch.randn(4, 5), (None,), None)
         with pytest.raises(TensorError):
             check_tensor(torch.randn(4, 5), (4, 5, None), torch.float)
+
+
+@pytest.mark.parametrize("batch_size", list(range(10)))
+@pytest.mark.parametrize("num_channels", list(range(1, 65)))
+def test_pack_bool_tensor(batch_size: int, num_channels: int):
+    b = torch.randint(0, 2, (batch_size, num_channels), dtype=torch.bool)
+    x = pack_bool_tensor(b)
+    assert x.shape == (batch_size,)
+    assert x.dtype in (torch.uint8, torch.uint16, torch.uint32, torch.uint64)
+    assert x.device == b.device
+
+
+@pytest.mark.parametrize("batch_size", list(range(10)))
+@pytest.mark.parametrize("num_channels", list(range(1, 65)))
+@pytest.mark.parametrize("otype", [torch.bool, torch.int8, torch.uint8, torch.uint16, torch.int16, torch.int32, torch.float16, torch.float32])
+def test_unpack_bit_tensor(batch_size: int, num_channels: int, otype: torch.dtype):
+    dtype = smallest_unsigned_integer_dtype(num_channels)
+    x = torch.from_numpy(np.random.randint(0, torch.iinfo(dtype).max, size=(batch_size,), dtype=np.uint64)).to(dtype)
+    b = unpack_bit_tensor(x, num_channels)
+    assert b.shape == (batch_size, num_channels)
+    assert b.dtype == torch.bool
+    assert b.device == x.device
+    y = unpack_bit_tensor(x, num_channels, dtype=otype)
+    assert y.shape == (batch_size, num_channels)
+    assert y.dtype == otype
+    assert y.device == x.device
+    assert torch.all(y == b.to(otype))
+
+
+@pytest.mark.parametrize("batch_size", list(range(10)))
+@pytest.mark.parametrize("num_channels", list(range(1, 65)))
+def test_pack_then_unpack_tensor(batch_size: int, num_channels: int):
+    b_1 = torch.randint(0, 2, (batch_size, num_channels), dtype=torch.bool)
+    x = pack_bool_tensor(b_1)
+    b_2 = unpack_bit_tensor(x, num_channels)
+    assert b_1.shape == b_2.shape
+    assert b_1.dtype == b_2.dtype
+    assert b_1.device == b_2.device
+    assert torch.all(b_1 == b_2)
