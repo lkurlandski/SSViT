5e4990021770e89b974adcf3e966a4ac08f9d5d4
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2025-11-06 12:30:38 -0500

    Changed the test data directory.

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,8 +10,10 @@ import os
 NUM_FILES = int(os.environ.get("SSVIT_TESTS_NUM_FILES", "8"))
 FILES = sorted(
     chain(
-        filter(lambda f: f.is_file(), Path("./data/ass").rglob("*")),
-        filter(lambda f: f.is_file(), Path("./data/sor").rglob("*")),
+        filter(lambda f: f.is_file(), Path("./datafiles/ass").rglob("*")),
+        filter(lambda f: f.is_file(), Path("./datafiles/sor").rglob("*")),
     ),
     key=lambda f: f.name
 )[0:NUM_FILES]
+if not FILES:
+    raise RuntimeError("No test files found in ./datafiles/")
