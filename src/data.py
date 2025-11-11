Semantic guides are now cast to the correct dtype for mixed precision training.

src/data.py
@@ -181,14 +181,18 @@ class _SemanticGuideOrSemanticGuides(ABC):
             characteristics = characteristics.clone()
         return self.__class__(parse, entropy, characteristics)
 
-    def to(self, device: torch.device, non_blocking: bool = False) -> Self:
+    def to(self, device: Optional[torch.device], dtype: Optional[torch.dtype] = None, non_blocking: bool = False) -> Self:
         parse, entropy, characteristics = self.parse, self.entropy, self.characteristics
         if parse is not None:
-            parse = parse.to(device, non_blocking=non_blocking)
+            if self.is_bitpacked and dtype is not None:
+                warnings.warn("Converting bitpacked tensor's dtype.")
+            parse = parse.to(device, dtype, non_blocking=non_blocking)
         if entropy is not None:
-            entropy = entropy.to(device, non_blocking=non_blocking)
+            entropy = entropy.to(device, dtype, non_blocking=non_blocking)
         if characteristics is not None:
-            characteristics = characteristics.to(device, non_blocking=non_blocking)
+            if self.is_bitpacked and dtype is not None:
+                warnings.warn("Converting bitpacked tensor's dtype.")
+            characteristics = characteristics.to(device, dtype, non_blocking=non_blocking)
         return self.__class__(parse, entropy, characteristics)
 
     def pin_memory(self) -> Self:
@@ -738,8 +742,12 @@ class _FSampleOrSamples(Generic[F, N, G, S], ABC):
         structure = self.structure.clone()
         return self.__class__(file, name, label, inputs, guides, structure)
 
-    def finalize(self, device: torch.device) -> Self:
-        return self.to(device, non_blocking=True).decompress()
+    def finalize(self, device: torch.device, ftype: torch.dtype) -> Self:
+        new: Self = self
+        new = new.to(device, non_blocking=True)
+        new = new.decompress()
+        new.guides = new.guides.to(None, ftype)
+        return new
 
 
 class FSample(_FSampleOrSamples[StrPath, Name, SemanticGuide, StructureMap]):
@@ -929,8 +937,12 @@ class _HSampleOrSamples(Generic[F, N, G, S], ABC):
         structure = self.structure.clone()
         return self.__class__(file, name, label, inputs, guides, structure)
 
-    def finalize(self, device: torch.device) -> Self:
-        return self.to(device, non_blocking=True).decompress()
+    def finalize(self, device: torch.device, ftype: torch.dtype) -> Self:
+        new: Self = self
+        new = new.to(device, non_blocking=True)
+        new = new.decompress()
+        new.guides = [g.to(None, ftype) for g in new.guides]
+        return new
 
 
 class HSample(_HSampleOrSamples[StrPath, Name, SemanticGuide, StructureMap]):

src/trainer.py
@@ -100,7 +100,7 @@ class Batch(Protocol):
         """
         ...
 
-    def finalize(self, device: torch.device) -> Self:
+    def finalize(self, device: torch.device, ftype: torch.dtype) -> Self:
         """
         Finalize the batch for use (e.g., move to device, change data types, etc.).
         """
@@ -365,7 +365,7 @@ class Trainer:
         self.scheduler = scheduler if scheduler is not None else LambdaLR(self.optimizer, lambda _: 1.0)
         self.stopper = stopper if stopper is not None else EarlyStopper(patience=float("inf"))
         self.device = device if device is not None else next(self.model.parameters()).device
-        self.padbatch = padbatch.finalize(self.device) if padbatch is not None else None
+        self.padbatch = padbatch.finalize(self.device, mp_dtype(self.args.mp16, self.device)) if padbatch is not None else None
         self.monitor = Monitor(device=self.device)
         self.log: list[Mapping[str, int | float]] = []
         self.glbl_step = 0
@@ -497,7 +497,7 @@ class Trainer:
             t_detailed_2 = time.time()
             t_detailed_preps.append(t_detailed_2 - t_detailed_1)
 
-            batch = batch.finalize(self.device)
+            batch = batch.finalize(self.device, mp_dtype(self.args.mp16, self.device))
 
             _syncronize_if_detailed_timing()
             t_detailed_3 = time.time()
@@ -617,7 +617,7 @@ class Trainer:
 
         with torch.no_grad():
             for mini_step, batch in iterable:
-                batch = batch.finalize(self.device)
+                batch = batch.finalize(self.device, mp_dtype(self.args.mp16, self.device))
                 real = mini_step < len(dataloader)
                 outputs = self.forward(batch)
                 loss = self.compute_loss(batch, outputs)