15be94d713a1e5a023d231eb20c0dce802f0fef4
Luke Kurlandski <lk3591@g.rit.edu>
2025-12-05 14:07:55 -0500

    Downsize guide dimension from 64 to 16.

--- src/main.py
+++ src/main.py
@@ -166,7 +166,7 @@ def get_model(
 
     # FiLM
     guide_dim    = num_guides
-    guide_hidden = 64
+    guide_hidden = 16
     FiLMCls: type[FiLM | FiLMNoP]
     if num_guides > 0:
         FiLMCls = FiLM
