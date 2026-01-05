a70c47657f63e9e743beff7f875fc05595513aab
Luke Kurlandski <lk3591@gcis-mkwics-rl.rit.edu>
2026-01-05 12:32:38 -0500

    Significant enhancements to the quick analysis script.

--- scripts/quickanal.py
+++ scripts/quickanal.py
@@ -6,10 +6,16 @@ from argparse import ArgumentParser
 import json
 from pathlib import Path
 
+import pandas as pd
+
 parser = ArgumentParser()
+parser.add_argument("input", type=str, nargs="?", default=None)
 parser.add_argument("--resfile", type=Path)
 parser.add_argument("--logfile", type=Path)
 parser.add_argument("--jobid", type=str)
+parser.add_argument("--json", action="store_true")
+parser.add_argument("--detailed", action="store_true")
+parser.add_argument("--quiet", action="store_true")
 parser.add_argument("--verbose", action="store_true")
 args = parser.parse_args()
 
@@ -18,9 +24,33 @@ LOGS = ROOT / "logs"
 BULK = Path("/shared/rc/admalware/Documents/code/SSViT")
 
 if args.verbose:
+    print(f"{args.input=}")
     print(f"{args.resfile=}")
     print(f"{args.logfile=}")
     print(f"{args.jobid=}")
+    print(f"{args.detailed=}")
+    print(f"{args.quiet=}")
+    print(f"{args.summary=}")
+
+if args.input is not None:
+    if (p := Path(args.input)).exists():
+        with open(p) as fp:
+            for line in fp:
+                try:
+                    json.loads(line.strip())
+                    if args.verbose:
+                        print(f"Input inferred to be resfile.")
+                    args.resfile = p
+                except:
+                    if args.verbose:
+                        print(f"Input inferred to be logfile.")
+                    args.logfile = p
+    elif args.input.isdigit():
+        if args.verbose:
+            print(f"Input inferred to be jobid.")
+        args.jobid = args.input
+    else:
+        raise RuntimeError(f"Could not infer the type of generic input {args.input}.")
 
 # Get the logfile, if not provided.
 if args.jobid is not None:
@@ -71,12 +101,35 @@ if args.verbose:
     print(f"Found {len(log)} entries.")
 
 # (lower-is-better, best-epoch, value)
-best = {
+best: dict[str, tuple[bool, int, float]] = {
     "tr_loss": (True, -1, float("inf")),
     "vl_loss": (True, -1, float("inf")),
     "vl_roc": (False, -1, -float("inf")),
     "vl_prc": (False, -1, -float("inf")),
 }
+
+def float_print_json(d: dict[str, float]) -> None:
+    print("{", end="")
+    for i, (k, v) in enumerate(d.items()):
+        if i != 0:
+            print(", ", end="")
+        print(f'"{k}": {v:.4f}', end="")
+    print("}")
+
+
+if args.detailed:
+    if not args.quiet:
+        print("Training Details:")
+    if args.json:
+        for d in log:
+            if not all(k in d for k in best):
+                continue
+            d = {k: d[k] for k in best}
+            float_print_json(d)
+    else:
+        df = pd.DataFrame(log)
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+            print(df[[k for k in best]])
 
 for d in log:
     for k in best:
@@ -98,14 +151,14 @@ for d in log:
         if args.verbose:
             print(f"({best[k]})")
 
-d = {k: v[1] for k, v in best.items()}
-print(d)
-
-d = {k: v[2] for k, v in best.items()}
-print("{", end="")
-for i, (k, v) in enumerate(d.items()):
-    if i != 0:
-        print(", ", end="")
-    print(f'"{k}": {v:.4f}', end="")
-print("}")
+if not args.quiet:
+    print("Training Summary:")
+if args.json:
+    d = {k: e for k, (l, e, v) in best.items()}
+    print(d)
+    d = {k: v for k, (l, e, v) in best.items()}
+    float_print_json(d)
+else:
+    for k, (l, e, v) in best.items():
+        print(f"{k} @{e} {v:.4f}")
 
